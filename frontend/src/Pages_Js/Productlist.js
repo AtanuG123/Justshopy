96d82b901191eb574bffd16041bf8739a5b11427
AtanuG123 <109225904+AtanuG123@users.noreply.github.com>
2023-09-10 13:32:22 +0530

    Update Productlist.js

--- frontend/src/Pages_Js/Productlist.js
+++ frontend/src/Pages_Js/Productlist.js
@@ -18,7 +18,7 @@ export default function Productlist() {
     useEffect(() => {
         Setisloading(true);
         document.getElementById("productlist").style.display = "none";
-        axios.post('https://justshopy-backend.vercel.app/productlist', { Catagory, Flag })
+        axios.post(`${process.env.REACT_APP_PORT}/productlist`, { Catagory, Flag })
             .then(res => {
                 setlistpro(res.data);
                 console.log(res.data)
